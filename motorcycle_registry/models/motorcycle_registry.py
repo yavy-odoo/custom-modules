2238726cdb88ea73718ae683cf5bf8ca0af4619c
yavy-odoo <yavy@odoo.com>
2023-06-28 12:11:53 -0400

    [ADD] constraints check - advanced ORM

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -1,5 +1,6 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import re
 
 class MotorcycleRegistry(models.Model):
     _name = "motorcycle.registry"
@@ -15,3 +16,22 @@ class MotorcycleRegistry(models.Model):
     license_plate = fields.Char("License Plate")
     certificate_title = fields.Binary("Certification Title")
     register_date = fields.Date("Registration Date")
+
+    @api.constrains('registry_number')
+    def _check_registry_number(self):
+        if not re.match(r"^MRN\d{4}$", self.registry_number):
+            raise ValidationError("Invalid format for Registration Number")
+
+    @api.constrains('vin')
+    def _check_vin(self):
+        if not re.match(r"^[A-Z]{4}\d{2}[A-Z0-9]{2}\d{6}$", self.vin):
+            raise ValidationError("Invalid format for VIN")
+
+        # check if VIN is unique
+        if self.search_count([('vin', '=', self.vin)]) > 1:
+            raise ValidationError("VIN already exists")
+
+    @api.constrains('license_plate')
+    def _check_license_plate(self):
+        if not re.match(r"^[a-zA-Z]{1,4}\d{1,3}[A-Z]{0,2}$", self.license_plate):
+            raise ValidationError("Invalid format for License Plate")
