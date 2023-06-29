260296ff65a8603dc3e274410522c5f9c74426ff
yavy-odoo <yavy@odoo.com>
2023-06-29 12:43:47 -0400

    [FIX] Motorcycle product model and form view

--- motorcycle_registry/models/product_template.py
+++ motorcycle_registry/models/product_template.py
@@ -9,19 +9,24 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     # override field
-    detailed_type = fields.Selection([
+    detailed_type = fields.Selection(selection_add=[
         ('motorcycle', 'Motorcycle'),
-        ('parts', 'Parts')], string='Product Type',
-        default='motorcycle', ondelete={'motorcycle': 'set product'})
+        ('parts', 'Parts')], ondelete={'motorcycle': 'set product', 'parts': 'set product'})
 
     # new fields
-    horsepower = fields.Integer()
+    horsepower = fields.Integer("Horsepower")
     top_speed = fields.Integer()
     torque = fields.Integer()
     battery_capacity = fields.Float()
     charge_time = fields.Float()
-    range =  fields.Float()
+    range =  fields.Float("Range")
     curb_weight =  fields.Float()
-    make = fields.Char()
-    model = fields.Char()
+    make = fields.Char("Make")
+    model = fields.Char("Model")
+
+    def _detailed_type_mapping(self):
+        type_mapping = super()._detailed_type_mapping()
+        type_mapping['motorcycle'] = 'product'
+        type_mapping['parts'] = 'product'
+        return type_mapping
 
