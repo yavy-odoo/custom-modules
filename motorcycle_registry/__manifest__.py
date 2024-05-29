1f16dc8a7c92f36c3442da60d428f781ed42e4f9
yavy-odoo <yavy@odoo.com>
2024-05-29 16:20:48 -0400

    [ADD] motorcycle_registry module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Motorcycle Registry',
+    'version': '0.0.1',
+    'description': 'Motorcyle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'author': 'yavy-odoo',
+    'website': 'github.com/yavy-odoo',
+    'license': 'LGPL-3',
+    'category': 'Kawiil/Custom Modules',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {
+        },
+    'icon': '/motorcycle_registry/static/description/icon-registration.png',
+}
