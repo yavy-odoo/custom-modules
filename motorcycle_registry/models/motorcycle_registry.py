9b9b405202a7fc5c83885f4a4a4f1ab4b1e91275
yavy-odoo <yavy@odoo.com>
2024-05-30 16:36:10 -0400

    [ADD] motorcycle_registry: advanced orm validation

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -1,9 +1,12 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+import re
 
 class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
     _description = 'Motorcycle Registry'
     _rec_name = 'registry_number' # Displayed in the title of a record view
+    _sql_constraints = [('unique_vin', 'unique(vin)', 'VIN must be unique')]
 
 
     certificate_title = fields.Binary('Certificate Title')
@@ -12,5 +15,28 @@ class MotorcycleRegistry(models.Model):
     first_name = fields.Char('First Name', required=True)
     last_name = fields.Char('Last Name', required=True)
     license_plate = fields.Char('License Plate')
-    registry_number = fields.Char('Registry Number', required=True, index=True)
+    registry_number = fields.Char('Registry Number', required=True, index=True, default="MRN0001", copy=False, readonly=True)
     vin = fields.Char('VIN', required=True)
+
+    @api.constrains('vin')
+    def _check_vin(self):
+        for record in self:
+            if not re.match(r"^[A-Z]{4}\d{2}[A-Z0-9]{2}\d{5}$", self.vin):
+                raise ValidationError('VIN must be in the format: WXYZ99XY99999')
+
+            if self.env['motorcycle.registry'].search_count([('vin', '=', self.vin)]) > 1:
+                raise ValidationError('VIN must be unique')
+
+    @api.constrains('license_plate')
+    def _check_license_plate(self):
+        for record in self:
+            if not re.match(r"^[A-Z]{1,4}[0-9]{1-3}[A-Z]{0,2}$", self.license_plate):
+                raise ValidationError('License Plate must be in the format: ABCD123 or ABCD1234 or ABCD123AB')
+
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('registry_number', 'MRN0001') == 'MRN0001':
+                vals['registry_number'] = self.env['ir.sequence'].next_by_code('motorcycle.registry') or 'MRN0001'
+        return super().create(vals_list)
