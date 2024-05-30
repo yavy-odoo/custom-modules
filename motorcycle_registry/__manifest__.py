63fc7bd1defbca57b9835b2189d07b8c95a68dfb
yavy-odoo <yavy@odoo.com>
2024-05-30 15:07:19 -0400

    [ADD] motorcycle_registry: security groups, menuitem

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -9,13 +9,16 @@
     'category': 'Kawiil/Custom Modules',
     'depends': ['base'],
     'data': [
-    ],
+        'security/motorcycle_registry_groups.xml',
+        'security/ir.model.access.csv',
+        'views/motorcycle_menuitems.xml',
     'demo': [
+        'data/demo.xml'
     ],
     'installable': True,
     'auto_install': False,
     'application': True,
     'assets': {
         },
-    'icon': '/motorcycle_registry/static/description/icon-registration.png',
+    'icon': '/motorcycle_registry/static/description/icon.png',
 }
