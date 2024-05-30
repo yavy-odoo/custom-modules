957eb16e627bf5df363e3f754794276189868584
yavy-odoo <yavy@odoo.com>
2024-05-30 15:28:37 -0400

    [ADD] motorcycle_registry: views

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -3,14 +3,14 @@ from odoo import fields, models
 class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
     _description = 'Motorcycle Registry'
-    _rec_name = 'registry_number'
+    _rec_name = 'registry_number' # Displayed in the title of a record view
 
 
-    certificate_title = fields.Binary('certificate_title')
-    current_mileage = fields.Float('current_mileage')
-    date_registry = fields.Date('date_registry')
-    first_name = fields.Char('first_name', required=True)
-    last_name = fields.Char('last_name', required=True)
-    license_plate = fields.Char('license_plate')
-    registry_number = fields.Char('registry_number', required=True, index=True)
-    vin = fields.Char('vin', required=True)
+    certificate_title = fields.Binary('Certificate Title')
+    current_mileage = fields.Float('Current Mileage')
+    date_registry = fields.Date('Date Registry')
+    first_name = fields.Char('First Name', required=True)
+    last_name = fields.Char('Last Name', required=True)
+    license_plate = fields.Char('License Plate')
+    registry_number = fields.Char('Registry Number', required=True, index=True)
+    vin = fields.Char('VIN', required=True)
